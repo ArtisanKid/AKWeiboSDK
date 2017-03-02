39513dfc873206dfd49dedca16f70350cbe24809
Freud <lixiangyujiayou@gmail.com>
2017-03-02 23:34:20 +0800

    Update:升级基础库

---
+++ AKWeiboSDK/Classes/AKWeiboSDK.h
@@ -0,0 +1,20 @@
+//
+//  AKWeiboSDK.h
+//  Pods
+//
+//  Created by 李翔宇 on 2017/2/15.
+//
+//
+
+/**
+ 文件说明
+ 此类没有任何实际意义，目的是为了使AKWeiboSDK具有源文件，能够生成Framework
+ 
+ 使用方需要复制Library Search Path和Other Linked Flag到自己的配置文件中
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface AKWeiboSDK : NSObject
+
+@end
---
+++ AKWeiboSDK/Classes/AKWeiboSDK.m
@@ -0,0 +1,13 @@
+//
+//  AKWeiboSDK.m
+//  Pods
+//
+//  Created by 李翔宇 on 2017/2/15.
+//
+//
+
+#import "AKWeiboSDK.h"
+
+@implementation AKWeiboSDK
+
+@end
